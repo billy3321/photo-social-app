10cbb3388f6cbc45a503393be439adda831e2666
billy3321 <billy3321@gmail.com>
2014-11-04 16:50:24 +0800

    try to do some omniauth for fb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141104073218) do
+ActiveRecord::Schema.define(version: 20141104083047) do
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20141104073218) do
     t.datetime "updated_at"
     t.string   "provider"
     t.string   "uid"
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true

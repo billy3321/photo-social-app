889433ddf07885a8870dc9bb934d2734ffb73fe3
billy3321 <billy3321@gmail.com>
2014-11-05 12:21:02 +0800

    use setting logic to store fb setting

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -256,6 +256,6 @@ Devise.setup do |config|
   # When using omniauth, Devise cannot automatically set Omniauth path,
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = '/my_engine/users/auth'
-  config.omniauth :facebook, '583963241750288', '', :scope => 'email, name'
+  config.omniauth :facebook, Setting.omniauth_facebook.app_id,  Setting.omniauth_facebook.app_secret, :scope => 'email,user_likes,public_profile'
 
 end
